933062b22810665ce7407cec85d75d9c15061d84
cwkoss <chris@mckoss.com>
2024-01-15 22:59:31 -0800

    trying to get openai api function working

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -17,3 +17,25 @@ const logger = require("firebase-functions/logger");
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+const functions = require("firebase-functions");
+const { OpenAIApi } = require("openai");
+
+const apiKey = functions.config().openai.key;
+const openai = OpenAIApi(apiKey);
+
+exports.chatWithOpenAI = functions.https.onRequest(async (request, response) => {
+    try {
+        const inputText = request.body.text || "what is the meaning of life?"; // Fallback text
+
+        const gptResponse = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: inputText,
+            max_tokens: 150,
+        });
+
+        response.send({ reply: gptResponse.data.choices[0].text });
+    } catch (error) {
+        console.error("Error calling OpenAI: ", error);
+        response.status(500).send("Error processing your request");
+    }
+});
