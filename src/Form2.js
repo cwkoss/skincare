f84b553106264b88a5eb84ba750cf22396f9d3ef
cwkoss <chris@mckoss.com>
2024-01-18 01:26:24 -0800

    reduce product types so they'll fit on mobile screen (and I don't know how to make all of them anyways)

--- src/Form2.js
+++ src/Form2.js
@@ -7,19 +7,20 @@ function Form2({ setForm2Data }) {
     const [selectedProduct, setSelectedProduct] = useState('');
 
     const skincareProducts = {
-        cleanser: "Used to remove dirt, oil, and makeup from the skin. It helps in keeping the pores clear and the skin clean.",
-        toner: "Applied after cleansing, toner helps to remove any remaining impurities and balances the pH of the skin.",
-        serum: "Highly concentrated with active ingredients, serums target specific skin concerns such as aging, hydration, or brightening.",
-        moisturizer: "Hydrates the skin and locks in moisture. It can come in various forms like lotions, creams, and gels.",
-        sunscreen: "Protects the skin from harmful UV rays. It's essential for preventing sun damage, aging, and skin cancer.",
+
+        //toner: "Applied after cleansing, toner helps to remove any remaining impurities and balances the pH of the skin.",
+        //serum: "Highly concentrated with active ingredients, serums target specific skin concerns such as aging, hydration, or brightening.",
+        //moisturizer: "Hydrates the skin and locks in moisture. It can come in various forms like lotions, creams, and gels.",
+        //sunscreen: "Protects the skin from harmful UV rays. It's essential for preventing sun damage, aging, and skin cancer.",
         // exfoliant: "Removes dead skin cells from the surface of the skin, promoting a smoother and clearer complexion.",
-        faceOil: "Provides intensive hydration and nourishment, especially beneficial for dry or mature skin.",
         //eyeCream: "Specifically formulated for the delicate eye area, it targets concerns like dark circles, puffiness, and wrinkles.",
         //mask: "Used occasionally to address specific skin concerns. Masks can hydrate, detoxify, or provide anti-aging benefits.",
         lotion: "A lightweight moisturizer, often used for normal to oily skin, providing hydration without being too heavy.",
         cream: "Thicker than lotion, creams are ideal for dry skin as they provide more intensive moisturization.",
         butter: "Even thicker than creams, butters are used for very dry skin areas, providing deep nourishment and hydration.",
-        balm: "A dense, oil-based product, balms are used for intense healing and protection, often in specific areas."
+        balm: "A dense, oil-based product, balms are used for intense healing and protection, often in specific areas.",
+        faceOil: "Provides intensive hydration and nourishment, especially beneficial for dry or mature skin.",
+        cleanser: "Used to remove dirt, oil, and makeup from the skin. It helps in keeping the pores clear and the skin clean.",
     };
 
     const handleSelection = (product) => {
