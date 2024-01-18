ce5581d56b4eaacd3d9d3bb4153559ec38273c61
cwkoss <chris@mckoss.com>
2024-01-18 03:39:49 -0800

    put ingredient selection and proprortion selection on separate pages

--- src/RecipeBuilder.js
+++ src/RecipeBuilder.js
@@ -5,8 +5,17 @@ function RecipeBuilder() {
     const [selectedIngredients, setSelectedIngredients] = useState([]);
     const [ingredientProportions, setIngredientProportions] = useState({});
     const [showIngredientsList, setShowIngredientsList] = useState(false);
+    const [currentStep, setCurrentStep] = useState('selectIngredients');
 
+    // Function to move to the proportion adjustment step
+    const goToProportionAdjustment = () => {
+        setCurrentStep('adjustProportions');
+    };
 
+    // Function to go back to ingredient selection
+    const goBackToSelectIngredients = () => {
+        setCurrentStep('selectIngredients');
+    };
 
     const handleIngredientSelect = (ingredientName) => {
         if (selectedIngredients.includes(ingredientName)) {
@@ -131,35 +140,35 @@ function RecipeBuilder() {
         });
 
         // Handle negative proportions
-  let negativeSum = 0;
-  nonAdditiveKeys.forEach(key => {
-    if (newProportions[key] < 0) {
-      negativeSum += newProportions[key];
-      newProportions[key] = 0;
-    }
-  });
+        let negativeSum = 0;
+        nonAdditiveKeys.forEach(key => {
+            if (newProportions[key] < 0) {
+                negativeSum += newProportions[key];
+                newProportions[key] = 0;
+            }
+        });
 
-  // Redistribute the negative sum among positive non-additive ingredients
-  if (negativeSum < 0) {
-    const positiveKeys = nonAdditiveKeys.filter(key => newProportions[key] >= 0.01);
-    const positiveAdjustment = Math.abs(negativeSum) / positiveKeys.length;
+        // Redistribute the negative sum among positive non-additive ingredients
+        if (negativeSum < 0) {
+            const positiveKeys = nonAdditiveKeys.filter(key => newProportions[key] >= 0.01);
+            const positiveAdjustment = Math.abs(negativeSum) / positiveKeys.length;
 
-    positiveKeys.forEach(key => {
-      newProportions[key] += positiveAdjustment;
-    });
-  }
+            positiveKeys.forEach(key => {
+                newProportions[key] += positiveAdjustment;
+            });
+        }
 
-      // Final check if total exceeds 100%
-      const finalTotal = Object.values(newProportions).reduce((acc, val) => acc + val, 0);
-      if (finalTotal > 100) {
-          const excess = finalTotal - 100;
-          const positiveKeys = nonAdditiveKeys.filter(key => newProportions[key] > 0);
-          const excessAdjustment = excess / positiveKeys.length;
-  
-          positiveKeys.forEach(key => {
-              newProportions[key] -= excessAdjustment;
-          });
-      }
+        // Final check if total exceeds 100%
+        const finalTotal = Object.values(newProportions).reduce((acc, val) => acc + val, 0);
+        if (finalTotal > 100) {
+            const excess = finalTotal - 100;
+            const positiveKeys = nonAdditiveKeys.filter(key => newProportions[key] > 0);
+            const excessAdjustment = excess / positiveKeys.length;
+
+            positiveKeys.forEach(key => {
+                newProportions[key] -= excessAdjustment;
+            });
+        }
 
         setIngredientProportions(newProportions);
     };
@@ -232,74 +241,79 @@ function RecipeBuilder() {
 
     return (
         <div>
-            <h2>Recipe Builder</h2>
-            <button onClick={() => setShowIngredientsList(!showIngredientsList)}>
-                {showIngredientsList ? 'Hide Ingredients' : 'Choose Ingredients'}
-            </button>
-
-            {showIngredientsList && (
+            {currentStep === 'selectIngredients' && (
                 <div>
-                    <h3>Available Ingredients:</h3>
-                    {Object.entries(ingredients).map(([name, details]) => (
-                        <div
-                            key={name}
-                            style={{ ...ingredientRowStyle, ...(isIngredientSelected(name) ? selectedStyle : {}) }}
-                            onClick={() => handleIngredientSelect(name)}
-                        >
-                            <strong>{name}</strong>
-                            <small>{details.description}</small>
-                        </div>
-                    ))}
+                    <div>
+                        <h3>Available Ingredients:</h3>
+                        {Object.entries(ingredients).map(([name, details]) => (
+                            <div
+                                key={name}
+                                style={{ ...ingredientRowStyle, ...(isIngredientSelected(name) ? selectedStyle : {}) }}
+                                onClick={() => handleIngredientSelect(name)}
+                            >
+                                <strong>{name}</strong>
+                                <small>{details.description}</small>
+                            </div>
+                        ))}
+                    </div>
+                    <button onClick={goToProportionAdjustment}>Adjust Proportions</button>
+                </div>
+            )}
+            {currentStep === 'adjustProportions' && (
+                <div>
+                    <button onClick={goBackToSelectIngredients}>Back to Ingredients</button>
+                    <div style={headerStyle}>
+                        <h3>Selected Ingredients:</h3>
+                        <span style={totalPercentageStyle}>{totalPercentage.toFixed(2)}%</span>
+                    </div>
+                    <table style={tableStyle}>
+                        <tbody>
+                            {selectedIngredients.map(name => (
+                                <React.Fragment key={name}>
+                                    <tr>
+                                        <td>{name}</td>
+                                        <td>{ingredientProportions[name].toFixed(2)}%</td>
+                                    </tr>
+                                    <tr>
+                                        <td colSpan="2">
+                                            {isAdditive(name) ? (
+                                                <div style={{ position: 'relative' }}>
+                                                    <input
+                                                        type="text"
+                                                        value={temporaryInputs[name] ?? ingredientProportions[name]}
+                                                        onChange={(e) => handleAdditiveChange(name, e.target.value)}
+                                                        onBlur={(e) => handleAdditiveBlur(name, e.target.value)}
+                                                        style={{ width: '100%', borderColor: ingredientProportions[name] === ingredients[name].max_percent ? 'red' : 'initial' }}
+                                                    />
+                                                    {ingredientProportions[name] === ingredients[name].max_percent && (
+                                                        <span style={{ position: 'absolute', right: 0, top: 0, color: 'red' }}>Max</span>
+                                                    )}
+                                                </div>
+                                            ) : (
+                                                <input
+                                                    type="range"
+                                                    min="0"
+                                                    max="100"
+                                                    step="0.01"
+                                                    value={ingredientProportions[name]}
+                                                    onChange={(e) => handleSliderChange(name, e.target.value)}
+                                                    onMouseUp={handleMouseUp}
+                                                    onTouchEnd={handleMouseUp}
+                                                    style={{ width: '100%' }}
+                                                />
+                                            )}
+                                        </td>
+                                    </tr>
+                                </React.Fragment>
+                            ))}
+
+                        </tbody>
+                    </table>
                 </div>
             )}
 
-            <div style={headerStyle}>
-                <h3>Selected Ingredients:</h3>
-                <span style={totalPercentageStyle}>{totalPercentage.toFixed(2)}%</span>
-            </div>
-            <table style={tableStyle}>
-                <tbody>
-                    {selectedIngredients.map(name => (
-                        <React.Fragment key={name}>
-                            <tr>
-                                <td>{name}</td>
-                                <td>{ingredientProportions[name].toFixed(2)}%</td>
-                            </tr>
-                            <tr>
-                                <td colSpan="2">
-                                    {isAdditive(name) ? (
-                                        <div style={{ position: 'relative' }}>
-                                            <input
-                                                type="text"
-                                                value={temporaryInputs[name] ?? ingredientProportions[name]}
-                                                onChange={(e) => handleAdditiveChange(name, e.target.value)}
-                                                onBlur={(e) => handleAdditiveBlur(name, e.target.value)}
-                                                style={{ width: '100%', borderColor: ingredientProportions[name] === ingredients[name].max_percent ? 'red' : 'initial' }}
-                                            />
-                                            {ingredientProportions[name] === ingredients[name].max_percent && (
-                                                <span style={{ position: 'absolute', right: 0, top: 0, color: 'red' }}>Max</span>
-                                            )}
-                                        </div>
-                                    ) : (
-                                        <input
-                                            type="range"
-                                            min="0"
-                                            max="100"
-                                            step="0.01"
-                                            value={ingredientProportions[name]}
-                                            onChange={(e) => handleSliderChange(name, e.target.value)}
-                                            onMouseUp={handleMouseUp}
-                                            onTouchEnd={handleMouseUp}
-                                            style={{ width: '100%' }}
-                                        />
-                                    )}
-                                </td>
-                            </tr>
-                        </React.Fragment>
-                    ))}
 
-                </tbody>
-            </table>
+
         </div>
     );
 }
