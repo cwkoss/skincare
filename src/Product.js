ea131a7d22405a15d719d33e0e02dec13769eda1
cwkoss <chris@mckoss.com>
2024-01-30 17:18:36 -0800

    update product descriptions

--- src/Product.js
+++ src/Product.js
@@ -7,7 +7,8 @@ function Product({ setProductData }) {
 
     const skincareProducts = {
         "Daytime Face Moisturizing Cream with SPF": "An invigorating morning cream that hydrates your skin while providing sun protection with natural zinc oxide, perfect for applying after your morning cleanse to keep your skin soft and shielded throughout the day.",
-        "Face Moisturizing Cream (No SPF)": "Ideal for night-time nourishment or for daytime use under your preferred sunscreen, this cream deeply moisturizes your skin, leaving it feeling smooth and refreshed",
+        "Face Moisturizing Cream (No SPF)": "Ideal for night-time nourishment or for daytime use under your preferred sunscreen, design a cream that will deeply moisturizes your skin, leaving it feeling smooth and refreshed",
+        "Body Moisturizing Cream or Body Butter": "Build a deeply hydrading and nourishing to lock in moisture and rejuvenate the skin. Body moisturizers are typically richer and thicker in texture than facial moisturizers, designed to hydrate the less sensitive, more robust skin of the body.",
         //toner: "Applied after cleansing, toner helps to remove any remaining impurities and balances the pH of the skin.",
         //serum: "Highly concentrated with active ingredients, serums target specific skin concerns such as aging, hydration, or brightening.",
         //moisturizer: "Hydrates the skin and locks in moisture. It can come in various forms like lotions, creams, and gels.",
@@ -19,8 +20,8 @@ function Product({ setProductData }) {
         //"Cream": "Thicker than lotion, creams are ideal for dry skin as they provide more intensive moisturization.",
         //"Butter": "Even thicker than creams, butters are used for very dry skin areas, providing deep nourishment and hydration.",
         //"Balm": "A dense, oil-based product, balms are used for intense healing and protection, often in specific areas.",
-        "Face Oil": "Designed to deeply nourish and rejuvenate your skin, best used in combination with a separate SPF product during the day to ensure full sun protection while maintaining radiant skin.",
-        "Scalp Oil":"A soothing and revitalizing scalp oil formulated to nourish your scalp and strengthen hair roots, perfect for daily use to maintain a healthy, hydrated scalp environment.",
+        "Face Oil": "Formulate an oil to deeply nourish and rejuvenate your skin, best used in combination with a separate SPF product during the day to ensure full sun protection while maintaining radiant skin.",
+        "Hair and Scalp Oil":"Formulate a soothing and revitalizing hair and scalp oil to nourish your scalp, strengthen hair roots, and add a healthy shine to your hair.  Use as a pre-shampoo treatment or as a leave-in conditioner.",
         //cleanser: "Used to remove dirt, oil, and makeup from the skin. It helps in keeping the pores clear and the skin clean.",
     };
 
