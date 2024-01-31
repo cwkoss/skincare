b4882ffad116d4651aa6d4efe2cb819e750b37ce
cwkoss <chris@mckoss.com>
2024-01-30 16:51:44 -0800

    Add cost per g to ingredients

--- src/ingredients.js
+++ src/ingredients.js
@@ -4,56 +4,81 @@ const ingredients = {
         hlb: 0,
         description: "Moisturizer, rich in Vitamin E, non-comedogenic, suitable for sensitive skin.",
         good_for: ["Sensitive Skin", "Dry skin"],
-        bad_for: []
+        bad_for: [],
+        cost: "$20/half gallon",
+        density: 0.92,
+        cost_per_g: 0.01148,
     },
     "Jojoba Oil": {
         phase: "oil",
         hlb: 0,
         description: "Moisturizer, mimics natural skin oils, balances sebum production, good for acne-prone skin.",
         good_for: ["Adult acne", "Dry skin", "Sensitive Skin"],
-        bad_for: []
+        bad_for: [],
+        cost: "$9.99/8oz",
+        density: 0.87,
+        cost_per_g: 0.0485,
     },
     "Argan Oil": {
         phase: "oil",
         hlb: 11,
         description: "Rich in antioxidants and Vitamin E, moisturizes, reduces signs of aging, improves skin elasticity and enhances hair shine.",
         good_for: ["Aging or age spots", "Dry skin", "Wrinkles"],
-        bad_for: []
+        bad_for: [],
+        cost: "$54.99/32oz",
+        density: 0.91,
+        cost_per_g: 0.0639,
+        buy_again_url:"https://www.organicpureoil.com/product/pure-1-oz-organic-argan-oil-extra-virgin-unrefined-cold-pressed/",
     },
     "Shea Butter": {
         phase: "oil",
         hlb: 8,
         description: "Moisturizer, rich in fatty acids and vitamins, anti-inflammatory, helps with skin healing.",
         good_for: ["Dry skin", "Atopic dermatitis"],
-        bad_for: ["Adult acne"] // due to its comedogenic properties
+        bad_for: ["Adult acne"], // due to its comedogenic properties
+        cost: "$9.99/16oz",
+        density: 0.927,
+        cost_per_g: 0.0228,
     },
     "Cocoa Butter": {
         phase: "oil",
         hlb: 8,
         description: "Moisturizer, high in fatty acids, helps to hydrate and nourish the skin, reduces the appearance of scars.",
         good_for: ["Dry skin", "Wrinkles"],
-        bad_for: ["Adult acne"]
+        bad_for: ["Adult acne"],
+        cost: "$1/oz",
+        cost_per_g: 0.0338,
+        buy_again_url: "https://simply-ingredients.com/collections/butters-waxes/products/fair-trade-cocoa-butter?variant=37889946484922",
     },
     "Coconut Oil": {
         phase: "oil",
         hlb: 8,
         description: "Moisturizes skin and hair, rich in fatty acids and antioxidants. Can be comedogenic (acne-causing) for some.",
         good_for: ["Dry skin"],
-        bad_for: ["Adult acne"] // high comedogenic rating
+        bad_for: ["Adult acne"], // high comedogenic rating
+        cost: "$6.96/15oz",
+        density: 0.924,
+        cost_per_g: 0.0170,
     },
     "Pecan Oil": {
         phase: "oil",
         hlb: 0,
         description: "Moisturizer, rich in antioxidants, promotes skin and hair health, hydrating properties.",
         good_for: ["Dry skin"],
-        bad_for: []
+        bad_for: [],
+        cost: "$17/8oz",
+        density: 0.95,
+        cost_per_g: 0.0756,
     },
     "Pumpkin Seed Oil": {
         phase: "oil",
         hlb: 0,
         description: "Rich in zinc and vitamins, improves skin tone, fights acne, and soothes sensitive skin.",
         good_for: ["Adult acne", "Sensitive Skin"],
-        bad_for: []
+        bad_for: [],
+        cost: "$3/2oz",
+        density: 0.95,
+        cost_per_g: 0.0534
     },
     /*"Benne Seed Oil": {
         phase: "oil",
@@ -74,7 +99,10 @@ const ingredients = {
         hlb: 0,
         description: "Moisturizer, rich in vitamins and antioxidants, promotes skin and hair health, but can be comedogenic (acne-causing) for some.",
         good_for: ["Dry skin"],
-        bad_for: ["Adult acne"]
+        bad_for: ["Adult acne"],
+        cost: "$18.99/68oz",
+        density: 0.9,
+        cost_per_g: 0.0105,
     },
     /*"Almond Oil": {
         phase: "oil",
@@ -88,14 +116,16 @@ const ingredients = {
         hlb: 20,
         description: "Hydrates and serves as a base for water-soluble ingredients.",
         good_for: [],
-        bad_for: []
+        bad_for: [],
+        cost_per_g: 0.0001,
     },
     "Green Tea": {
         phase: "aqueous",
         hlb: 20,
         description: "Antioxidant-rich, reduces inflammation and redness, can protect against environmental stressors and promotes skin healing.",
         good_for: ["Redness", "Puffiness", "Sensitive Skin"],
-        bad_for: []
+        bad_for: [],
+        cost_per_g: 0.0070,
     },
     "Sea Salt": {
         phase: "aqueous",
@@ -109,14 +139,20 @@ const ingredients = {
         hlb: 20,
         description: "Soothes skin irritations, hydrates, promotes healing, good for sunburns and inflammation",
         good_for: ["Sensitive Skin", "Sun Protection", "Redness"],
-        bad_for: []
+        bad_for: [],
+        cost: "9.99/300ml",
+        density: 1.2,
+        cost_per_g: 0.02775 
     },
     "Glycerin": {
         phase: "aqueous",
         hlb: 20,
         description: "Humectant, draws moisture into the skin, suitable for all skin types, enhances skin hydration.",
         good_for: ["Dry skin"],
-        bad_for: []
+        bad_for: [],
+        cost: "3.66/4oz",
+        density: 1.26,
+        cost_per_g: 0.0246 
     },
     "Honey": {
         phase: "aqueous",
@@ -151,28 +187,33 @@ const ingredients = {
         hlb: 12,
         description: " Emulsifier and stabilizer in formulations, creates a protective barrier on the skin, retains moisture.",
         good_for: ["Dry skin"],
-        bad_for: []
+        bad_for: [],
+        cost_per_g: 0.1706
     },
     "Lecithin": {
         phase: "emulsifier",
         hlb: 8,
         description: "Natural stabilizer, thickens formulas and creates protective barrier on skin.",
         good_for: [],
-        bad_for: []
+        bad_for: [],
+        cost_per_g: 0.0884,
     },
     "Cetearyl Alcohol": {
         phase: "emulsifier",
         hlb: 16,
         description: "A waxy fatty alcohol used as an emulsifier, thickener and stabilizer in creams and lotions. Extracted from Coconut and Palm Kernel oils",
         good_for: [],
-        bad_for: ["Sensitive Skin"] // can be irritating for some sensitive skins
+        bad_for: ["Sensitive Skin"], // can be irritating for some sensitive skins
+        cost_per_g: 0.0439,
     },
     "Castille Soap": {
         phase: "surfactant",
         hlb: "10",
         description: "Gentle and versatile cleanser, made from olive and hemp oils, suitable for sensitive skin.",
         good_for: ["Sensitive Skin"],
-        bad_for: []
+        bad_for: [],
+        density: 1.04,
+        cost_per_g: 0.0172,
     },
     "Zinc Oxide": {
         phase: "additive",
@@ -181,7 +222,8 @@ const ingredients = {
         default_percent: 10,
         description: "Sunscreen agent, provides broad-spectrum UV protection, also known for its soothing and astringent properties.",
         good_for: ["Sun Protection"],
-        bad_for: []
+        bad_for: [],
+        cost_per_g: 0.0958,
     },
     "Hyaluronic Acid": {
         phase: "additive",
@@ -190,7 +232,8 @@ const ingredients = {
         default_percent: 0.5,
         description: "Powerful humectant, helps to hydrate and plump the skin by retaining moisture, reduces the appearance of fine lines and wrinkles.",
         good_for: ["Dry skin", "Wrinkles", "Aging or age spots"],
-        bad_for: []
+        bad_for: [],
+        cost_per_g: 0.4639,
     },
     "Retinol": {
         phase: "additive",
@@ -199,7 +242,8 @@ const ingredients = {
         default_percent: 0.1,
         description: "A form of Vitamin A, helps in skin renewal, boosts collagen production, effective against aging signs. Should be used intermittently until skin aclimates to it. Sun sensitizing, so should only be used at night.",
         good_for: ["Wrinkles", "Aging or age spots"],
-        bad_for: ["Sensitive Skin"] // can be irritating
+        bad_for: ["Sensitive Skin"], // can be 
+        cost_per_g: 0.4282,
     },
     "Vitamin C": {
         phase: "additive",
@@ -208,7 +252,8 @@ const ingredients = {
         default_percent: 5,
         description: "Antioxidant, aids in skin repair and regeneration, can help in reducing pigmentation and promoting collagen production.",
         good_for: ["Aging or age spots", "Sun Protection"],
-        bad_for: []
+        bad_for: [],
+        cost_per_g: 0.0288,
     },
     "Vitamin E": {
         phase: "additive",
@@ -217,7 +262,9 @@ const ingredients = {
         default_percent: 0.5,
         description: "Antioxidant, helps in protecting the skin from environmental damage, moisturizes, and heals. Extends shelf life of oils.",
         good_for: ["Sun Protection", "Aging or age spots"],
-        bad_for: []
+        bad_for: [],
+        density: 0.95,
+        cost_per_g: 0.3505,
     },
     "Niacinamide (Vitamin B3)": {
         phase: "additive",
@@ -226,7 +273,8 @@ const ingredients = {
         default_percent: 2,
         description: "Improves appearance of enlarged pores, uneven skin tone, fine lines. Reduces redness and irritation.",
         good_for: ["Large pores", "Wrinkles", "Redness"],
-        bad_for: []
+        bad_for: [],
+        cost_per_g: 0.3567,
     },
     "Lavender Oil": {
         phase: "additive",
@@ -235,7 +283,9 @@ const ingredients = {
         default_percent: 0.5,
         description: "Calming and relaxing, and can be used to soothe irritated skin. Anti-inflammatory and antimicrobial properties.",
         good_for: ["Sensitive Skin", "Redness"],
-        bad_for: []
+        bad_for: [],
+        density: 0.885,
+        cost_per_g: 1.1578,
     },
     "Rosemary Oil": {
         phase: "additive",
@@ -244,7 +294,9 @@ const ingredients = {
         default_percent: 1,
         description: "Rejuvenates skin, improves hair growth and scalp health, and has antioxidant properties. Improves circulation and can help with puffiness.",
         good_for: ["Aging or age spots"],
-        bad_for: ["Sensitive Skin"] // can be irritating for some people
+        bad_for: ["Sensitive Skin"], // can be irritating for some people
+        density: 0.895,
+        cost_per_g: 0.1214
     },
     "Tea Tree Oil": {
         phase: "additive",
@@ -253,7 +305,9 @@ const ingredients = {
         default_percent: 1,
         description: "Antimicrobial and anti-inflammatory, very effective against acne, helps in cleansing the skin.",
         good_for: ["Adult acne"],
-        bad_for: ["Dry skin"] // can be drying if overused
+        bad_for: ["Dry skin"], // can be drying if overused
+        density: 0.878,
+        cost_per_g: 0.3792,
     },
     "Jasmine Oil": {
         phase: "additive",
@@ -262,7 +316,9 @@ const ingredients = {
         default_percent: 0.5,
         description: "Moisturizes, soothes, often used for its fragrance and hydrating properties.",
         good_for: ["Dry skin"],
-        bad_for: []
+        bad_for: [],
+        density: 0.947,
+        cost_per_g: 2.7434, // expensive
     },
     "Eucalyptus Oil": {
         phase: "additive",
@@ -271,7 +327,9 @@ const ingredients = {
         default_percent: 1,
         description: "Antiseptic and anti-inflammatory properties, good for healing. Refreshing scent.",
         good_for: ["Adult acne"],
-        bad_for: ["Sensitive Skin", "Dry skin"] // can be irritating and drying
+        bad_for: ["Sensitive Skin", "Dry skin"], // can be irritating and drying
+        densitY: 0.914 ,
+        cost_per_g: 0.1530,
     },
     "Peppermint Oil": {
         phase: "additive",
@@ -280,7 +338,9 @@ const ingredients = {
         default_percent: 0.5,
         description: "Cooling sensation and refreshing scent. It has antiseptic and antimicrobial properties. Can help with acne and oily skin, but can be irritating.",
         good_for: ["Adult acne", "Puffiness"],
-        bad_for: ["Sensitive Skin", "Redness"] // can be irritating and cause a burning sensation for sensitive skin types
+        bad_for: ["Sensitive Skin", "Redness"], // can be irritating and cause a burning sensation for sensitive skin types
+        density: 0.898,
+        cost_per_g: 0.0942,
     },
     "Pine Oil": {
         phase: "additive",
@@ -289,7 +349,8 @@ const ingredients = {
         default_percent: 0.5,
         description: "Known for its invigorating and refreshing scent, has antimicrobial and anti-inflammatory properties. It's often used in skincare for its soothing effects on the skin and its ability to help with respiratory issues.",
         good_for: ["Atopic dermatitis", "Adult acne"],
-        bad_for: ["Sensitive Skin"] // can be irritating due to its potent nature
+        bad_for: ["Sensitive Skin"], // can be irritating due to its potent nature
+        cost_per_g: 1.155,
     },
     "Cedar Oil": {
         phase: "additive",
@@ -298,7 +359,9 @@ const ingredients = {
         default_percent: 0.5,
         description: "With its woodsy aroma, is known for its calming and grounding effects. It has antiseptic, anti-inflammatory, and astringent properties, making it beneficial for skin conditions like acne and aiding in soothing irritated skin.",
         good_for: ["Adult acne", "Atopic dermatitis"],
-        bad_for: ["Sensitive Skin"] // may cause irritation in higher concentrations or sensitive individuals
+        bad_for: ["Sensitive Skin"], // may cause irritation in higher concentrations or sensitive individuals
+        density: 0.952,
+        cost_per_g: 1.7507
     },
     "Rosehip Seed Oil": {
         phase: "oil",
@@ -307,7 +370,9 @@ const ingredients = {
         default_percent: 2,
         description: "Rosehip Seed Oil is rich in essential fatty acids and antioxidants. It's renowned for its skin-regenerative properties, aiding in reducing scars and fine lines. High in vitamins A and C, it helps in evening out skin tone and improving hydration.",
         good_for: ["Wrinkles", "Dry skin", "Sun Protection", "Aging or age spots"],
-        bad_for: [] // Generally well-tolerated, but always patch test
+        bad_for: [], // Generally well-tolerated, but always patch test
+        density: 0.92,
+        cost_per_g: 0.6159,
     },
 
     "Geranium Oil": {
@@ -317,7 +382,9 @@ const ingredients = {
         default_percent: 0.5,
         description: "Geranium Oil, known for its floral scent, is used for its astringent, antiseptic, and anti-inflammatory properties. It helps in balancing sebum production, enhancing skin elasticity, and promoting the healing of scars and spots.",
         good_for: ["Adult acne", "Large pores", "Wrinkles"],
-        bad_for: ["Sensitive Skin"] // Can be irritating for some sensitive skin types
+        bad_for: ["Sensitive Skin"], // Can be irritating for some sensitive skin types
+        density: 0.887,
+        cost_per_g: 1.5031,
     },
 
     "Carrot Seed Oil": {
@@ -327,7 +394,9 @@ const ingredients = {
         default_percent: 1,
         description: "Carrot Seed Oil is valued for its antioxidant properties and high vitamin A content. It's considered excellent for sun protection, rejuvenating aged skin, and promoting skin healing. Also known for its ability to rejuvenate and regenerate skin tissues.",
         good_for: ["Aging or age spots", "Sun Protection", "Wrinkles"],
-        bad_for: ["Sensitive Skin"] // Can be potent; patch test recommended
+        bad_for: ["Sensitive Skin"], // Can be potent; patch test recommended
+        density: 0.92,
+        cost_per_g: 1.5942
     },
     /*"Cedarwood Oil": {
         phase: "additive",
@@ -365,7 +434,7 @@ const ingredients = {
         good_for: ["Redness", "Adult acne", "Atopic dermatitis"],
         bad_for: []
     },
-    "Cinnamon": {
+    /* "Cinnamon": {
         phase: "additive",
         hlb: 0,
         max_percent: 0.5,
@@ -391,7 +460,7 @@ const ingredients = {
         description: "Nutmeg, with its anti-inflammatory and antibacterial properties, is used in small amounts to soothe skin and improve its appearance.",
         good_for: ["Redness", "Atopic dermatitis"],
         bad_for: ["Sensitive Skin"] // can be irritating in higher concentrations
-    },
+    },*/
     "Tepezcohuite Mimosa Tenuiflora Infusion": {
         phase: "aqueous",
         hlb: 0, // Typically, herbal infusions do not have an HLB value
