ef197373e6b4f5cbbbba3cc5741d9023c322acc5
cwkoss <chris@mckoss.com>
2024-06-18 17:35:36 -0700

    confirmrecipe formatting

--- src/ConfirmRecipe.js
+++ src/ConfirmRecipe.js
@@ -3,29 +3,36 @@ import Layout from './Layout';
 import { useRecipe } from './RecipeContext';
 
 function ConfirmRecipe() {
-  const { state } = useRecipe();
+    const { state } = useRecipe();
 
-  return (
-    <Layout title="Confirm Your Recipe">
-      <div>
-        <h2>Recipe Confirmation</h2>
-        {JSON.stringify(state)}
-        {state.recipe && Object.keys(state.recipe).length > 0 ? (
-          Object.keys(state.recipe).map((phase, index) => (
-            <div key={index}>
-              <h3>{phase.charAt(0).toUpperCase() + phase.slice(1)} Phase</h3>
-              <p><strong>Title:</strong> {state.recipe[phase].title}</p>
-              <p><strong>Ingredients:</strong> {state.recipe[phase].ingredients ? Object.entries(state.recipe[phase].ingredients).map(([key, value]) => `${key}: ${value}`).join(', ') : 'No ingredients listed'}</p>
-              <p><strong>Description:</strong> {state.recipe[phase].description}</p>
+    const goToNextPhase = () => {
+        alert("Recipe Confirmed!")
+    }
+
+    return (
+        <Layout title="Confirm Your Recipe"
+            buttonText="Confirm Recipe"
+            handleSubmit={() => { goToNextPhase() }}>
+            <div>
+                {state.recipe && Object.keys(state.recipe).length > 0 ? (
+                    Object.keys(state.recipe).map((phase, index) => (
+                        <div key={index}>
+                            <h3>{phase.charAt(0).toUpperCase() + phase.slice(1)} Phase</h3>
+                            <p><strong>Title:</strong> {state.recipe[phase].title}</p>
+                            <p><strong>Ingredients:</strong> {state.recipe[phase].ingredients ? Object.entries(state.recipe[phase].ingredients).map(([key, value]) => `${key}: ${value}`).join(', ') : 'No ingredients listed'}</p>
+                            <p><strong>Description:</strong> {state.recipe[phase].description}</p>
+                        </div>
+                    ))
+                ) : (
+                    <p>No recipe data available.</p>
+                )}
+
+                debugdump
+                {JSON.stringify(state.recipe)}
+
             </div>
-          ))
-        ) : (
-          <p>No recipe data available.</p>
-        )}
-        <button onClick={() => alert("Recipe Confirmed!")}>Confirm Recipe</button>
-      </div>
-    </Layout>
-  );
+        </Layout>
+    );
 }
 
 export default ConfirmRecipe;
